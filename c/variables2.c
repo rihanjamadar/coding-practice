a3db78cc0c0e10602965f4e2690c00717c450b6d
Rihanjamadar <jamadarrihan33@gmail.com>
2026-09-26 19:14:33 +0530

    learnt about C

---
+++ c/variables2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int i = 10;
+    int j = i+10 ;
+    printf("The value of i is %d and value of j is %d", i, j); // %d is used as format specifier
+    return 0;
+
+}
